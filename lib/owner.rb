0a43e453ed431233c0c4ebee75f85693324a8ee2
Joann Pan <joannlpan@gmail.com>
2020-05-14 19:26:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,13 +64,21 @@ class Owner
     end
   end
 
+  def pets 
+    @pets = self.dogs + self.cats
+  end
+    
+
   def sell_pets
-    pets = self.dogs + self.cats
-    pets.each do |pet|
+    @pets.each do |pet|
       pet.mood = "nervous"
       pet.owner = nil
     end
   end
+
+  def list_pets
+    
+  
 
 
 
