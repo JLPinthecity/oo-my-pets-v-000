fae83375eea52864a8b289c45917fc1a542d587a
Joann Pan <joannlpan@gmail.com>
2020-05-14 18:35:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,20 @@
 class Owner
   attr_reader :name
 
+@@all = []
+
   def initialize
     @species = "human"
+    save
   end
 
   def say_species
     @species
   end
+
+  def save
+    @@all << self
+  end
+
+  def 
 end
